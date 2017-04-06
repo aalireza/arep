da957e509cb9f833638724e077c1874ffff20539
Alireza Rafiei <alireza@rafiei.net>
2017-04-06 12:13:14 -0700

    Adding Comparison evaluator and mappings from operators to ast nodes

--- higher_grep/__init__.py
+++ higher_grep/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 from collections import namedtuple
 from functools import partial
+from inspect import getfullargspec as spec
 import ast
 import os
 import sys
@@ -8,6 +9,369 @@ import sys
 if sys.version_info > (3, 0):
     long = int
 
+def _Constraints_template():
+    return {
+        'Action': {
+            'Call': {
+                'should_consider': None,
+            },
+            'Initialization': {
+                'should_consider': None,
+            },
+            'Import': {
+                'should_consider': None,
+                'id': None,
+                'from': None,
+                'from_id': None,
+                'as': None,
+            },
+            'Definition': {
+                'should_consider': None,
+            },
+            'Assignment': {
+                'should_consider': None,
+                'with_op': None,
+            },
+            'Assertion': {
+                'should_consider': None,
+                'with_error_msg': None,
+                'error_msg_content': None,
+            },
+            'Looping': {
+                'should_consider': None,
+                'for': None,
+                'while': None,
+                'for_else': None,
+                'with_break': None,
+                'with_non_terminating_test': None,
+            },
+            'Conditional': {
+                'should_consider': None,
+                'with_elif': None,
+                'with_else': None,
+                'is_ifexp': None,
+            },
+            'With': {
+                'should_consider': None,
+                'as': None,
+            },
+            'Deletion': {
+                'should_consider': None,
+            },
+            'Indexing': {
+                'should_consider': None,
+            },
+            'Trying': {
+                'should_consider': None,
+                'with_all_except_types': None,
+                'without_any_except_types': None,
+                'with_except_ases': None,
+                'with_finally': None,
+            },
+            'Raising': {
+                'should_consider': None,
+                'error_type': None,
+                'error_message': None,
+            },
+            'Yielding': {
+                'should_consider': None,
+                'in_comprehension': None,
+                'yield_from': None,
+            },
+            'Making_Global': {
+                'should_consider': None,
+                'id': None,
+            },
+            'Making_Nonlocal': {
+                'should_consider': None,
+                'id': None,
+            }
+        },
+        'Kind': {
+            'Variables': {
+                'should_consider': None,
+                'id': None,
+                'is_attribute': None,
+                'in_global': None,
+            },
+            'STD_Types': {
+                'should_consider': None,
+                'type': None,
+            },
+            'Functions': {
+                'should_consider': None,
+                'id': None,
+                'in_global': None,
+                'is_lambda': None,
+                'is_decorator': None,
+            },
+            'Decorators': {
+                'should_consider': None,
+                'id': None,
+            },
+            'Classes': {
+                'should_consider': None,
+                'id': None,
+                'in_global': None,
+                'superclass_type': None,
+            },
+            'Attributes': {
+                'should_consider': None,
+                'id': None,
+                'class_id': None,
+            },
+            'Methods': {
+                'should_consider': None,
+                'id': None,
+                'class_id': None,
+            },
+            'Generators': {
+                'should_consider': None,
+                'id': None,
+            },
+            'Comprehensions': {
+                'should_consider': None,
+                'of_list': None,
+                'of_dict': None,
+                'of_gen': None,
+            },
+            'Operations': {
+                'should_consider': None,
+                'operation_str': None,
+                'is_unary': None,
+                'is_binary': None,
+            },
+        },
+        'Location_Limit': {
+            'Line_Numbers': {
+                'minimum': None,
+                'maximum': None,
+            },
+            'Column_Numbers': {
+                'minimum': None,
+                'maximum': None,
+            }
+        }
+    }
+
+
+def _establish_parent_link(tree):
+    to_be_processed = [tree]
+    while to_be_processed:
+        try:
+            current_parent = to_be_processed.pop(0)
+            children = current_parent.body
+            for child in children:
+                child._parent = current_parent
+            to_be_processed.extend(children)
+        except AttributeError:
+            if to_be_processed:
+                to_be_processed.pop(0)
+        except IndexError:
+            continue
+    if not to_be_processed:
+        return tree
+    raise Exception
+
+
+def count_arity(func):
+    if any(
+            [possibly_infinite is not None
+             for possibly_infinite in {spec(func).varargs, spec(func).varkw}]
+    ):
+        return float("inf")
+    return len(spec(func).args)
+
+
+def _ast_mapped_operators():
+
+    def basic_reducer(*args, definition=(lambda *args: None)):
+        results = set([])
+        current_element = args[0]
+        if count_arity(definition) == 1:
+            assert len(args) == 1
+            results.add(definition(current_element))
+        else:
+            assert len(args) > 1
+            for other_element in args[1:]:
+                results.add(definition(current_element, other_element))
+                current_element = other_element
+        return all(results)
+
+    ast_mappings = dict()
+
+    equality_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x == y)
+    )
+    ast_mappings[ast.Eq.__name__] = equality_definition_checker
+
+    inequality_definition_checker = (
+        lambda *args: not equality_definition_checker(*args)
+    )
+    ast_mappings[ast.NotEq.__name__] = inequality_definition_checker
+
+    less_than_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x < y)
+    )
+    ast_mappings[ast.Lt.__name__] = less_than_definition_checker
+
+    less_than_or_equal_definition_checker = (
+        lambda *args: (less_than_definition_checker(*args) or
+                       equality_definition_checker(*args))
+    )
+    ast_mappings[ast.LtE.__name__] = less_than_or_equal_definition_checker
+
+    greater_than_or_equal_definition_checker = (
+        lambda *args: not less_than_definition_checker(*args)
+    )
+    ast_mappings[ast.GtE.__name__] = greater_than_or_equal_definition_checker
+
+    greater_than_definition_checker = (
+        lambda *args: not less_than_or_equal_definition_checker(*args)
+    )
+    ast_mappings[ast.GtE.__name__] = greater_than_definition_checker
+
+    is_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: bool(x is y)))
+    ast_mappings[ast.Is.__name__] = is_definition_checker
+
+    is_not_definition_checker = (
+        lambda *args: not is_definition_checker(*args)
+    )
+    ast_mappings[ast.IsNot.__name__] = is_not_definition_checker
+
+    in_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: bool(x in y))
+    )
+    ast_mappings[ast.In.__name__] = in_definition_checker
+
+    not_in_definition_checker = (
+        lambda *args: not in_definition_checker(*args)
+    )
+    ast_mappings[ast.NotIn.__name__] = not_in_definition_checker
+
+    additive_identity_definition_checker = partial(
+        basic_reducer, definition=(lambda x: +x)
+    )
+    ast_mappings[ast.UAdd.__name__] = additive_identity_definition_checker
+
+    additive_inverse_definition_checker = partial(
+        basic_reducer, definition=(lambda x: -x)
+    )
+    ast_mappings[ast.USub.__name__] = additive_inverse_definition_checker
+
+    addition_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x + y)
+    )
+    ast_mappings[ast.Add.__name__] = addition_definition_checker
+
+    subtraction_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x - y)
+    )
+    ast_mappings[ast.Sub.__name__] = subtraction_definition_checker
+
+    multiplication_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x * y)
+    )
+    ast_mappings[ast.Mult.__name__] = multiplication_definition_checker
+
+    division_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x / y)
+    )
+    ast_mappings[ast.Div.__name__] = division_definition_checker
+
+    integer_division_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x // y)
+    )
+    ast_mappings[ast.FloorDiv.__name__] = integer_division_definition_checker
+
+    modulo_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x % y)
+    )
+    ast_mappings[ast.Mod.__name__] = modulo_definition_checker
+
+    exponentiation_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x ** y)
+    )
+    ast_mappings[ast.Pow.__name__] = exponentiation_definition_checker
+
+    matrix_multiplication_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x @ y)
+    )
+    ast_mappings[
+        ast.MatMult.__name__] = matrix_multiplication_definition_checker
+
+    boolean_not_definition_checker = partial(
+        basic_reducer, definition=(lambda x: not x)
+    )
+    ast_mappings[ast.Not.__name__] = boolean_not_definition_checker
+
+    boolean_and_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x and y)
+    )
+    ast_mappings[ast.And.__name__] = boolean_and_definition_checker
+
+    boolean_or_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x or y)
+    )
+    ast_mappings[ast.Or.__name__] = boolean_or_definition_checker
+
+    bitwise_inversion_definition_checker = partial(
+        basic_reducer, definition=(lambda x: ~x)
+    )
+    ast_mappings[ast.Invert.__name__] = bitwise_inversion_definition_checker
+
+    bitwise_left_shift_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x << y)
+    )
+    ast_mappings[ast.LShift.__name__] = bitwise_left_shift_definition_checker
+
+    bitwise_right_shift_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x >> y)
+    )
+    ast_mappings[ast.RShift.__name__] = bitwise_right_shift_definition_checker
+
+    bitwise_and_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x & y)
+    )
+    ast_mappings[ast.BitAnd.__name__] = bitwise_and_definition_checker
+
+    bitwise_or_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x | y)
+    )
+    ast_mappings[ast.BitOr.__name__] = bitwise_or_definition_checker
+
+    bitwise_xor_definition_checker = partial(
+        basic_reducer, definition=(lambda x, y: x ^ y)
+    )
+    ast_mappings[ast.BitXor.__name__] = bitwise_xor_definition_checker
+
+    return ast_mappings
+
+
+def _comparison_evaluator(node):
+
+    def type_seive(comparator):
+        if bool(type(comparator) is ast.Num):
+            return comparator.n
+        elif bool(type(comparator) is ast.Str):
+            return comparator.s
+        raise NotImplementedError(
+            "Comparator can't be {}".format(type(comparator))
+        )
+
+    if not bool(type(node) is ast.Compare):
+        raise TypeError("{} is not a comparator".format(node))
+    operations = _ast_mapped_operators()
+    left = type_seive(node.left)
+    for op in map(
+            lambda op: operations[type(op).__name__],
+            node.ops
+    ):
+        current_comparator = type_seive(node.comparators.pop(0))
+        left = op(left, current_comparator)
+
+    return left
 
 def _Constraints_template():
     return {
@@ -988,22 +1352,3 @@ class Grepper(object):
 
     def get_all_results(self):
         return list(self.run())
-
-
-if __name__ == '__main__':
-    # a = Grepper('/home/raf/Projects/Workspace/higher-grepy/data.py')
-    a = Grepper('/home/raf/Projects/Workspace/higher-grepy/tests/data/Action/Assignment.py')
-    # a.add_constraint(Kind.Functions())
-    # a.add_constraint(Kind.Functions(_is_lambda=True))
-    # a.add_constraint(Action.Call(should_consider=True))
-    # a.add_constraint(Action.Import(_from=True, _from_id='collections'))
-    # a.add_constraint(Action.Import(_id = 'deque'))
-    # a.add_constraint(Action.Import(_from=False))
-    # a.add_constraint(Action.Definition())
-    a.add_constraint(Action.Assignment())
-    # a.add_constraint(Action.Assertion())
-    # a.add_constraint(Action.Looping())
-    # a.add_constraint(Action.Conditional())
-    # a.add_constraint(Action.Making_Global())
-    # a.add_constraint(Action.Deletion())
-    print(a.get_all_results())
---
+++ tests/test_comparison_evaluator.py
@@ -0,0 +1,19 @@
+import higher_grep as hg
+import pytest
+
+
+ops = hg._ast_mapped_operators()
+
+
+@pytest.mark.parametrize(('args', 'result'), [
+    ([2, 1], False),
+    ([1, 2], False),
+    ([1, 1], True),
+    ([2, 2, 2, 2, 1], False),
+    ([2, 2, 2, 2, 2], True),
+    (["a", "a", "a"], True),
+    (["a", "b", "b"], False),
+    ([pytest, pytest], True)
+])
+def test_eq(args, result):
+    assert ops['Eq'](*args) == result
