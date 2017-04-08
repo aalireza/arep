1397a46c3f018f9d807204e2006dd175f12149b2
Alireza Rafiei <alireza@rafiei.net>
2017-04-08 06:26:14 -0700

    Reformatting Raising tests

--- tests/test_Action_Raising.py
+++ tests/test_Action_Raising.py
@@ -6,10 +6,13 @@ import os
 results_template = namedtuple('Raising', 'Line Column')
 
 
-results = {
-    results_template(x, y)
-    for x, y in {(3, 8), (5, 8), (10, 4), (12, 0)}
-}
+def results_formatter(results):
+    return {results_template(x, y) for x, y in results}
+
+
+all_results = results_formatter({
+    (3, 8), (5, 8), (10, 4), (12, 0)
+})
 
 
 @pytest.fixture
@@ -20,34 +23,34 @@ def grepper():
 
 def test_Raising(grepper):
     grepper.add_constraint(hg.Action.Raising())
-    assert results == set(grepper.get_all_results())
+    assert set(grepper.get_all_results()) == all_results
 
 
 @pytest.mark.parametrize(("_error_type", "result"), [
-    (TypeError, {results_template(5, 8)}),
-    (Exception, {results_template(3, 8), results_template(10, 4)}),
-    (SystemExit, {results_template(12, 0)}),
+    (TypeError, {(5, 8)}),
+    (Exception, {(3, 8), (10, 4)}),
+    (SystemExit, {(12, 0)}),
     (AttributeError, set([]))
 ])
 def test_error_type(grepper, _error_type, result):
     grepper.add_constraint(hg.Action.Raising(_error_type=_error_type))
-    assert set(grepper.get_all_results()) == result
+    assert set(grepper.get_all_results()) == results_formatter(result)
 
 
 @pytest.mark.parametrize(("_error_message", "result"), [
-    ("Something", {results_template(3, 8)}),
-    ("test", {results_template(10, 4)}),
+    ("Something", {(3, 8)}),
+    ("test", {(10, 4)}),
     ("Another test", set([]))
 ])
 def test_error_message(grepper, _error_message, result):
     grepper.add_constraint(hg.Action.Raising(_error_message=_error_message))
-    assert set(grepper.get_all_results()) == result
+    assert set(grepper.get_all_results()) == results_formatter(result)
 
 
 @pytest.mark.parametrize(("_cause", "result"), [
-    ("e", {results_template(10, 4)}),
+    ("e", {(10, 4)}),
     ("d", set([]))
 ])
 def test_cause(grepper, _cause, result):
     grepper.add_constraint(hg.Action.Raising(_cause=_cause))
-    assert set(grepper.get_all_results()) == result
+    assert set(grepper.get_all_results()) == results_formatter(result)
