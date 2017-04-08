eb357087cdc6e02ebd60f45396b9e21505e0c2a0
Alireza Rafiei <alireza@rafiei.net>
2017-04-08 06:27:08 -0700

    Reformatting STD_Types tests

--- tests/test_Kind_STD_Types.py
+++ tests/test_Kind_STD_Types.py
@@ -5,11 +5,15 @@ import os
 
 
 results_template = namedtuple('STD_Types', 'Line Column')
-all_results = {(1, 6), (7, 8), (9, 7), (9, 18)}
 
 
 def results_formatter(results):
     return {results_template(x, y) for x, y in results}
+
+
+all_results = results_formatter({
+    (1, 6), (7, 8), (9, 7), (9, 18)
+})
 
 
 @pytest.fixture
@@ -21,14 +25,14 @@ def grepper():
 
 def test_STD_Types(grepper):
     grepper.add_constraint(hg.Kind.STD_Types())
-    assert results_formatter(all_results) == set(grepper.get_all_results())
+    assert set(grepper.get_all_results()) == all_results
 
 
-@pytest.mark.parametrize(('_type', 'results'), [
+@pytest.mark.parametrize(('_type', 'result'), [
     (str, {(7, 8), (9, 18)}),
     (int, {(1, 6)}),
     (dict, set([])),
 ])
-def test_type(grepper, _type, results):
+def test_type(grepper, _type, result):
     grepper.add_constraint(hg.Kind.STD_Types(_type=_type))
-    assert results_formatter(results) == set(grepper.get_all_results())
+    assert set(grepper.get_all_results()) == results_formatter(result)
