c15d382bb052fc024e57f9de302a18302fa379c3
Alireza Rafiei <alireza@rafiei.net>
2017-04-08 06:26:55 -0700

    Reformatting Yielding tests

--- tests/test_Action_Yielding.py
+++ tests/test_Action_Yielding.py
@@ -5,23 +5,25 @@ import os
 
 results_template = namedtuple('Yielding', 'Line Column')
 
-results_in_comprehension = {
-    results_template(x, y)
-    for x, y in {(3, 5)}
-}
 
-results_regular = {
-    results_template(x, y)
-    for x, y in {(8, 8)}
-}
-
-results_yield_from = {
-    results_template(x, y)
-    for x, y in {(12, 4)}
-}
+def results_formatter(results):
+    return {results_template(x, y) for x, y in results}
 
 
-results = (results_in_comprehension | results_regular | results_yield_from)
+results_in_comprehension = results_formatter({
+    (3, 5)
+})
+
+results_regular = results_formatter({
+    (8, 8)
+})
+
+results_yield_from = results_formatter({
+    (12, 4)
+})
+
+
+all_results = (results_in_comprehension | results_regular | results_yield_from)
 
 
 @pytest.fixture
@@ -33,7 +35,7 @@ def grepper():
 
 def test_Yielding(grepper):
     grepper.add_constraint(hg.Action.Yielding())
-    assert results == set(grepper.get_all_results())
+    assert set(grepper.get_all_results()) == all_results
 
 
 @pytest.mark.parametrize(('_in_comprehension'), [True, False])
@@ -43,14 +45,17 @@ def test_in_comprehension(grepper, _in_comprehension):
     if _in_comprehension:
         assert results_in_comprehension == set(grepper.get_all_results())
     else:
-        assert (results - results_in_comprehension) == set(
-            grepper.get_all_results())
+        assert set(grepper.get_all_results()) == (
+            all_results - results_in_comprehension
+        )
 
 
 @pytest.mark.parametrize(('_yield_from'), [True, False])
 def test_yield_from(grepper, _yield_from):
     grepper.add_constraint(hg.Action.Yielding(_yield_from=_yield_from))
     if _yield_from:
-        assert results_yield_from == set(grepper.get_all_results())
+        assert set(grepper.get_all_results()) == results_yield_from
     else:
-        assert (results - results_yield_from) == set(grepper.get_all_results())
+        assert set(grepper.get_all_results()) == (
+            all_results - results_yield_from
+        )
