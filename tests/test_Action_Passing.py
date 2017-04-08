e6cde13c622cdc77251b86c77e399c297c7e9d08
Alireza Rafiei <alireza@rafiei.net>
2017-04-08 06:26:02 -0700

    Reformatting Passing tests

--- tests/test_Action_Passing.py
+++ tests/test_Action_Passing.py
@@ -5,18 +5,22 @@ import os
 
 results_template = namedtuple('Passing', 'Line Column')
 
-results = {results_template(x, y) for x, y in {
+
+def results_formatter(results):
+    return {results_template(x, y) for x, y in results}
+
+
+all_results = results_formatter({
     (2, 4), (10, 8)
-}}
+})
 
 
 @pytest.fixture
 def grepper():
-    engine = hg.Grepper(
-        os.path.abspath('tests/data/Action/Passing.py'))
+    engine = hg.Grepper(os.path.abspath('tests/data/Action/Passing.py'))
     return engine
 
 
 def test_Passing(grepper):
     grepper.add_constraint(hg.Action.Passing())
-    assert results == set(grepper.get_all_results())
+    assert all_results == set(grepper.get_all_results())
