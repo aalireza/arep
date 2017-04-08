4c96f327b83d1e0177b6e924b82a8a8744c8c6a8
Alireza Rafiei <alireza@rafiei.net>
2017-04-08 06:24:12 -0700

    Reformatting Conditional tests

--- tests/test_Action_Conditional.py
+++ tests/test_Action_Conditional.py
@@ -5,32 +5,33 @@ import os
 
 results_template = namedtuple('Conditional', 'Line Column')
 
-results_with_elif = {
-    results_template(x, y) for x, y in {
-        (2, 0), (13, 0)
-    }
-}
 
-results_with_else = {
-    results_template(x, y) for x, y in {
-        (2, 0), (11, 5)
-    }
-}
+def results_formatter(results):
+    return {results_template(x, y) for x, y in results}
 
-results_is_ifexp = {
-    results_template(x, y) for x, y in {
-        (11, 5)
-    }
-}
 
-results_in_comprehensions = {
-    results_template(x, y) for x, y in {
-        (18, 10), (20, 11), (23, 4), (23, 16)
-    }
-}
+results_with_elif = results_formatter({
+    (2, 0), (13, 0)
+})
 
-results = ({results_template(30, 4)} | results_with_elif | results_with_else |
-           results_is_ifexp | results_in_comprehensions)
+results_with_else = results_formatter({
+    (2, 0), (11, 5)
+})
+
+results_is_ifexp = results_formatter({
+    (11, 5)
+})
+
+results_in_comprehensions = results_formatter({
+    (18, 10), (20, 11), (23, 4), (23, 16)
+})
+
+misc_results = results_formatter({
+    (30, 4)
+})
+
+all_results = (misc_results | results_with_elif | results_with_else |
+               results_is_ifexp | results_in_comprehensions)
 
 
 @pytest.fixture
@@ -41,8 +42,7 @@ def grepper():
 
 def test_Conditional(grepper):
     grepper.add_constraint(hg.Action.Conditional())
-    print(results)
-    assert results == set(grepper.get_all_results())
+    assert set(grepper.get_all_results()) == all_results
 
 
 @pytest.mark.parametrize(('_with_elif'), [True, False])
@@ -51,7 +51,9 @@ def test_with_elif(grepper, _with_elif):
     if _with_elif:
         assert set(grepper.get_all_results()) == results_with_elif
     else:
-        assert set(grepper.get_all_results()) == (results - results_with_elif)
+        assert set(grepper.get_all_results()) == (
+            all_results - results_with_elif
+        )
 
 
 @pytest.mark.parametrize(('_with_else'), [True, False])
@@ -60,7 +62,9 @@ def test_with_else(grepper, _with_else):
     if _with_else:
         assert set(grepper.get_all_results()) == results_with_else
     else:
-        assert set(grepper.get_all_results()) == (results - results_with_else)
+        assert set(grepper.get_all_results()) == (
+            all_results - results_with_else
+        )
 
 
 @pytest.mark.parametrize(('_is_ifexp'), [True, False])
@@ -69,7 +73,9 @@ def test_is_ifexp(grepper, _is_ifexp):
     if _is_ifexp:
         assert set(grepper.get_all_results()) == results_is_ifexp
     else:
-        assert set(grepper.get_all_results()) == (results - results_is_ifexp)
+        assert set(grepper.get_all_results()) == (
+            all_results - results_is_ifexp
+        )
 
 
 @pytest.mark.parametrize(('_with_elif'), [True, False, None])
@@ -80,11 +86,11 @@ def test_combo(grepper, _with_elif, _with_else, _is_ifexp):
         _with_elif=_with_elif, _with_else=_with_else, _is_ifexp=_is_ifexp))
     obtained_results = set(grepper.get_all_results())
     if _is_ifexp is None:
-        target_results = results.copy()
+        target_results = all_results.copy()
     elif _is_ifexp is True:
         target_results = results_is_ifexp.copy()
     elif _is_ifexp is False:
-        target_results = (results - results_is_ifexp)
+        target_results = (all_results - results_is_ifexp)
     if _with_elif is True:
         target_results &= results_with_elif
     elif _with_elif is False:
