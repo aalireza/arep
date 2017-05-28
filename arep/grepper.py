c53466c07f4f43011dfd1927c7a3b3232e61be18
Alireza Rafiei <alireza@rafiei.net>
2017-05-28 00:18:49 -0700

    Renaming get_all_results to all_results and removing add_constraint method

--- arep/grepper.py
+++ arep/grepper.py
@@ -1,15 +1,50 @@
-from higher_grep.utils import establish_parent_link
-from higher_grep.utils import Knowledge_template
-from higher_grep.utils import update_knowledge_template
-from higher_grep.utils import Result
-from higher_grep import Validators
+from arep.utils import (
+    establish_parent_link, Knowledge_template, update_knowledge_template,
+    Result
+)
+from arep import Validators
 from functools import namedtuple, partial
 import ast
 import os
 
 
 class Grepper(object):
+    """
+    Attributes
+    ----------
+    constraint_list
+        The list of all the constraints that are being considered.
+        It automatically "AND"s all the constraints. For now assign
+        `consideration` to False for a constraint to `NOT` it and union
+        greppers for "OR".
+    __ast
+        The modified AST of the program where each node with a parent has a
+        pointer to it.
+    __source
+        str of the source code
+    __name
+        File name of the program
+    __knowledge_template
+        A dictionary of some of the knowledge that can be automatically gained
+        by looking at the entire AST. For example, it helps to see whether
+        `f()` is a class or a function or alike. It can't be known without it.
+
+    Methods
+    -------
+    get_source()
+    get_knowledge()
+    run()
+        A generator that yields nodes that satisfy all the constraints.
+    all_results()
+        Returns a list of all the results by exhusting the generator above.
+    """
     def __init__(self, source_abs_path):
+        """
+        Parameters
+        ----------
+        source_abs_path : str
+            Absolute path to the python source code that'll be parsed.
+        """
         assert os.path.exists(source_abs_path), "Path doesn't exist"
         with open(source_abs_path, 'r') as f:
             self.__ast = establish_parent_link(ast.parse(f.read()))
@@ -18,20 +53,34 @@ class Grepper(object):
             self.__knowledge_template = update_knowledge_template(
                 ast_tree_with_parent_pointers=self.__ast,
                 knowledge_template=Knowledge_template(),
-                results_name=self.__name
             )
             self.constraint_list = list()
 
     def get_source(self):
+        """
+        Returns
+        -------
+        str
+        """
         return self.__source
 
     def get_knowledge(self):
+        """
+        Returns
+        -------
+        {str: type}
+        """
         return self.__knowledge_template
 
-    def add_constraint(self, constraint):
-        self.constraint_list.append(constraint)
-
     def _validator_predicates_deriver(self):
+        """
+        Collects all of the functions that need to be checked on each node
+        for satisfaction.
+
+        Returns
+        -------
+        [(*args, **kwargs) -> {True, False}]
+        """
 
         def per_constraint_deriver(constraint):
 
@@ -75,6 +124,12 @@ class Grepper(object):
         ]
 
     def run(self):
+        """
+        Yields
+        ------
+        Result
+            An object with `name`, `line` and `column` attributes.
+        """
         validator_predicates = {
             validator_predicate
             for constraint_block in self._validator_predicates_deriver()
@@ -92,5 +147,10 @@ class Grepper(object):
                 except AttributeError:
                     pass
 
-    def get_all_results(self):
+    def all_results(self):
+        """
+        Returns
+        -------
+        [Result]
+        """
         return list(self.run())
